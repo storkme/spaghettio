d19cf30c723fb443acc341c91685d5c194af847f
storkme <storkme@users.noreply.github.com>
2026-08-21 13:37:18 +0000

    Deploy preview for PR #691 (747d7cbad0a67ac6e7f5bf103fa373ad18ba8b94)

--- pr-691/assets/BufferResource-J58Wbkom.js
+++ pr-691/assets/BufferResource-J58Wbkom.js
@@ -1,4 +1,4 @@
-import { y as U, B as g, D as c, F as S, H as _, J as m, E as I, K as p } from "./index-8qdP37tg.js";
+import { y as U, B as g, D as c, F as S, H as _, J as m, E as I, K as p } from "./index-CCBKp5Hg.js";
 const x = { name: "local-uniform-bit", vertex: { header: `
 
             struct LocalUniforms {
--- pr-691/assets/browserAll-B9nn21-i.js
+++ pr-691/assets/browserAll-B9nn21-i.js
@@ -1,6 +1,6 @@
-import { a4 as M, a5 as Z, a6 as m, a7 as te, P as y, E as ie, y as g, a3 as P, C as V } from "./index-8qdP37tg.js";
-import "./webworkerAll-DxNnAbOQ.js";
-import "./Filter-Dy18TNQV.js";
+import { a4 as M, a5 as Z, a6 as m, a7 as te, P as y, E as ie, y as g, a3 as P, C as V } from "./index-CCBKp5Hg.js";
+import "./webworkerAll-CoO_4Ha5.js";
+import "./Filter-CO8L1Njg.js";
 class q {
   constructor(e) {
     this._lastTransform = "", this._observer = null, this._tickerAttached = false, this.updateTranslation = () => {
